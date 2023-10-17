98aa70eb4b070c922beb8901a125e61abe8ea476
Navin Subramanian <assasin71003@gmail.com>
2023-10-17 20:47:06 +0530

    Style changes for all

--- TWIB1/urls.py
+++ TWIB1/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path('',views.index),
+    path('home/',views.index),
     path('men/',views.men),
     path('movie/',views.movie),
     path('women/',views.women),
